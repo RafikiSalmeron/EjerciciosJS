bc66355af1d53f03405ab25fe7b2b69f411d6da8
Rafa Salmeron <rafikisalmeronmartos@gmail.com>
2022-06-09 12:43:31 +0200

    Ejercicio 8

---
+++ Ejercicio 8/main.js
@@ -0,0 +1,68 @@
+// EJERCICIO 1 //
+let mes = 2;
+switch (mes) {
+  case 1:
+    console.log("Enero");
+    break;
+  case 2:
+    console.log("Febrero");
+    break;
+  case 3:
+    console.log("Marzo");
+    break;
+  case 4:
+    console.log("Abril");
+    break;
+  case 5:
+    console.log("Mayo");
+    break;
+  case 6:
+    console.log("Junio");
+    break;
+  case 7:
+    console.log("Julio");
+    break;
+  case 8:
+    console.log("Agosto");
+    break;
+  case 9:
+    console.log("Septiembre");
+    break;
+  case 10:
+    console.log("Octubre");
+    break;
+  case 11:
+    console.log("Noviembre");
+    break;
+  case 12:
+    console.log("Diciembre");
+    break;
+  default:
+    console.log("Mes inválido");
+    break;
+}
+
+// EJERCICIO 2 //
+let num = "tres";
+
+switch (num) {
+  case "uno":
+    num = 1;
+    break;
+  case "dos":
+    num = 2;
+    break;
+  case "tres":
+    num = 3;
+    break;
+  case "cuatro":
+    num = 4;
+    break;
+  case "cinco":
+    num = 5;
+    break;
+  default:
+    break;
+}
+
+console.log(num);
