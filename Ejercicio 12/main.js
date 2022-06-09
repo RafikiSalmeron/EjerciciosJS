0894581945096ef799cabc88071bfb8ac689ec76
Rafa Salmeron <rafikisalmeronmartos@gmail.com>
2022-06-09 13:14:39 +0200

    Ejercicio 12

---
+++ Ejercicio 12/main.js
@@ -0,0 +1,12 @@
+// EJERCICIO 1 //
+var array = ['a', 'b', 'c', 'd', 'e'];
+array.forEach(element => {
+    console.log(element);    
+});
+
+// EJERCICIO 2 //
+let numero = 5;
+while(numero >= 0){
+    console.log(numero);
+    numero--;
+}
