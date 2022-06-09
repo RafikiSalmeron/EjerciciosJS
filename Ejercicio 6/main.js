842748dfcc9d2af53b390b434a21e46640df9a00
Rafa Salmeron <rafikisalmeronmartos@gmail.com>
2022-06-09 11:50:30 +0200

    Ejercicio 6

---
+++ Ejercicio 6/main.js
@@ -0,0 +1,19 @@
+function ejer1 (num1, num2, num3){
+  let suma = num1 + num2 + num3;
+  return suma;
+}
+console.log(ejer1(1,2,3));
+
+function ejer2 (nombre, ape1, ape2){
+  let name = nombre + " " + ape1 + " " + ape2;
+  return name;
+}
+console.log("Rafael", "Salmeron", "Martos");
+
+function ejer3(num1, num2){
+  if(num1>num2){
+    return num1;
+  }
+  return num2;
+}
+console.log(ejer3(6,5));
