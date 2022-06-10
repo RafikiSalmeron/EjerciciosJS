1998d148b8e3687e6d9789c6be36f2a96e618bfb
Rafa Salmeron <rafikisalmeronmartos@gmail.com>
2022-06-10 09:43:33 +0200

    Ejercicio 15

---
+++ Ejercicio 15/quiz.js
@@ -0,0 +1,130 @@
+var quiz = {
+
+  data: [
+  {
+    q : "Pregunta de prueba 1",
+    o : [
+      "Esta si",
+      "Esta no",
+      "Esta no"
+    ],
+    a : 0 
+  },
+  {
+    q : "Pregunta de prueba 2",
+    o : [
+      "Esta no",
+      "Esta si",
+      "Esta no"
+    ],
+    a : 1
+  },
+  {
+    q : "Pregunta de prueba 3",
+    o : [
+      "Esta no",
+      "Esta no",
+      "Esta si"
+    ],
+    a : 2
+  }
+  ],
+
+  hWrap: null, // HTML quiz container
+  hQn: null, // HTML question wrapper
+  hAns: null, // HTML answers wrapper
+
+  // (A3) GAME FLAGS
+  now: 0, // current question
+  score: 0, // current score
+
+  // (B) INIT QUIZ HTML
+  init: () => {
+    // (B1) WRAPPER
+    quiz.hWrap = document.getElementById("quizWrap");
+
+    // (B2) QUESTIONS SECTION
+    quiz.hQn = document.createElement("div");
+    quiz.hQn.id = "quizQn";
+    quiz.hWrap.appendChild(quiz.hQn);
+
+    // (B3) ANSWERS SECTION
+    quiz.hAns = document.createElement("div");
+    quiz.hAns.id = "quizAns";
+    quiz.hWrap.appendChild(quiz.hAns);
+
+    // (B4) GO!
+    quiz.draw();
+  },
+
+  // (C) DRAW QUESTION
+  draw: () => {
+    // (C1) QUESTION
+    quiz.hQn.innerHTML = quiz.data[quiz.now].q;
+
+    // (C2) OPTIONS
+    quiz.hAns.innerHTML = "";
+    for (let i in quiz.data[quiz.now].o) {
+      let radio = document.createElement("input");
+      radio.type = "radio";
+      radio.name = "quiz";
+      radio.id = "quizo" + i;
+      quiz.hAns.appendChild(radio);
+      let label = document.createElement("label");
+      label.innerHTML = quiz.data[quiz.now].o[i];
+      label.setAttribute("for", "quizo" + i);
+      label.dataset.idx = i;
+      label.addEventListener("click", () => { quiz.select(label); });
+      quiz.hAns.appendChild(label);
+    }
+
+    //QUESTIONS COUNT
+    let label = document.createElement("label");
+    label.innerHTML = `Question ${quiz.now + 1} of ${quiz.data.length}`;
+    label.id ="label-contador"
+    quiz.hAns.appendChild(label);
+
+  },
+
+  // (D) OPTION SELECTED
+  select: (option) => {
+    // (D1) DETACH ALL ONCLICK
+    let all = quiz.hAns.getElementsByTagName("label");
+    for (let label of all) {
+      label.removeEventListener("click", quiz.select);
+    }
+
+    // (D2) CHECK IF CORRECT
+    let correct = option.dataset.idx == quiz.data[quiz.now].a;
+    if (correct) {
+      quiz.score++;
+      option.classList.add("correct");
+    } else {
+      option.classList.add("wrong");
+    }
+
+    // (D3) NEXT QUESTION OR END GAME
+    quiz.now++;
+    setTimeout(() => {
+      if (quiz.now < quiz.data.length) { quiz.draw(); }
+      else {
+        quiz.hQn.innerHTML = `You have answered ${quiz.score} of ${quiz.data.length} correctly.`;
+        quiz.hAns.innerHTML = "";
+
+        let label = document.createElement("label");
+        label.innerHTML = `RESET`;
+        label.id ="label-reset"
+        label.addEventListener("click", () => { quiz.reset(); });
+        quiz.hQn.appendChild(label);
+      }
+    }, 1000);
+  },
+
+  // (E) RESTART QUIZ
+  reset : () => {
+    quiz.now = 0;
+    quiz.score = 0;
+    quiz.draw();
+  }
+};
+window.addEventListener("load", quiz.init);
